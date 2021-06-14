2ca9c5df4468f969cf02164cfc3608556bae7707
Baris000-eng <63741832+Baris000-eng@users.noreply.github.com>
2021-06-14 21:39:48 +0300

    Update and rename KU ARCHITECT.java to ARCHITECT.java

--- ARCHITECT.java
+++ ARCHITECT.java
@@ -9,18 +9,18 @@
 import acm.program.ConsoleProgram;
 import acm.util.RandomGenerator;
 
-public class KUArchitect extends ConsoleProgram {
+public class Architect extends ConsoleProgram {
 
 	public void run() {
 		//RUN STARTS
-	for(currentKUArchitectID=1;currentKUArchitectID<=CONTESTANT_NUM;currentKUArchitectID++) {
+	for(currentArchitectID=1;currentArchitectID<=CONTESTANT_NUM;currentArchitectID++) {
 		println("NEW CONTESTANT: ");
-	    assignPoint(currentKUArchitectID);
+	    assignPoint(currentArchitectID);
 	    transitions();//This method puts spaces and lines between the contestants.
 	}         
-	    comparator();//This method compares the KUArchitects' total points and according to these points, this method prints in which place the  KUArchitect's are.
+	    comparator();//This method compares the Architects' total points and according to these points, this method prints in which place the  Architect's are.
 	    battleAndWinNumbers();
-	    congratulations();//This method prints out the congratulatory message for the winner of KUArchitect according to the winning point.
+	    congratulations();//This method prints out the congratulatory message for the winner of Architect according to the winning point.
 	    //RUN ENDS
     }
 
@@ -35,16 +35,16 @@ public class KUArchitect extends ConsoleProgram {
 	 */
 	private double askKnowledge() {
 		// your code starts here
-				int numberOfEducation= readInt("Enter years of education KUArchitect#"+currentKUArchitectID+": ");
+				int numberOfEducation= readInt("Enter years of education Architect#"+currentArchitectID+": ");
 				while(numberOfEducation<4||numberOfEducation>6) {//If the number of education is not in the suitable range, this method asks another number of education value from the suitable range.
-					numberOfEducation= readInt("Enter a valid number of years of education of KUArchitect#"+currentKUArchitectID+": " );
+					numberOfEducation= readInt("Enter a valid number of years of education of Architect#"+currentArchitectID+": " );
 					if(4<=numberOfEducation&&numberOfEducation<=6) {
 						break;
 					}
 				}
-			    int numberOfExperience= readInt("Enter years of experience KUArchitect#"+currentKUArchitectID+": ");
+			    int numberOfExperience= readInt("Enter years of experience Architect#"+currentArchitectID+": ");
 				while(numberOfExperience<2||numberOfExperience>10) {//If the number of experience is not in the suitable range, this method asks another number of experience value from the suitable range.
-					 numberOfExperience= readInt("Enter a valid number of years of experience of KUArchitect#"+currentKUArchitectID+" : ");	 
+					 numberOfExperience= readInt("Enter a valid number of years of experience of Architect#"+currentArchitectID+" : ");	 
 					 if(2<=numberOfExperience&&numberOfExperience<=10) {
 						 break;
 					 }
@@ -71,16 +71,16 @@ public class KUArchitect extends ConsoleProgram {
 	 */
 	private int askExperience() {
 		// your code starts here
-			int numOfProjects= readInt("Number of projects completed by KUArchitect#"+currentKUArchitectID+": ");
+			int numOfProjects= readInt("Number of projects completed by Architect#"+currentArchitectID+": ");
 			while(numOfProjects<4||numOfProjects>15) {
-		    numOfProjects= readInt("Enter a valid number of projects completed by KUArchitect#"+currentKUArchitectID+": " );
+		    numOfProjects= readInt("Enter a valid number of projects completed by Architect#"+currentArchitectID+": " );
 		        if(4<=numOfProjects&&numOfProjects<=15) {
 		        	break;
 		        }
 		    }
-		    int numOfDifProjects= readInt("Number of different projects of  KUArchitect#"+currentKUArchitectID+": ");
+		    int numOfDifProjects= readInt("Number of different projects of  Architect#"+currentArchitectID+": ");
 			while(numOfDifProjects<2||numOfDifProjects>5) {
-				numOfDifProjects= readInt("Enter a valid number of different projects of  KUArchitect#"+currentKUArchitectID+":");
+				numOfDifProjects= readInt("Enter a valid number of different projects of  Architect#"+currentArchitectID+":");
 				if(2<=numOfDifProjects&&numOfDifProjects<=5) {
 					break;
 		        }
@@ -107,9 +107,9 @@ public class KUArchitect extends ConsoleProgram {
 	
 	private int askAwards() {//In this method,I have requested the number of awards from the user.
 		//If the user enters a value out of the suitable boundaries for number of awards, I have requested the number of awards from the suitable range again.
-			int awards= readInt("Number of awards of KUArchitect#"+currentKUArchitectID+": ");
+			int awards= readInt("Number of awards of Architect#"+currentArchitectID+": ");
 			while(awards<1||awards>5) {
-				 awards= readInt("Enter a valid number of awards of KUArchitect#"+currentKUArchitectID+": ");
+				 awards= readInt("Enter a valid number of awards of Architect#"+currentArchitectID+": ");
 				 if(1<=awards&&awards<=5) {
 					 break;
 			     }
@@ -126,14 +126,7 @@ public class KUArchitect extends ConsoleProgram {
         return g;
 	}
 	
-	/**
-	 * This method calculates the total points of a KUArchitect
-	 * @param knowledge - the knowledge point of a KUArchietct
-	 * @param experience - the experience point of a KUArchietct
-	 * @param awards - the number of awards of a KUArchietct
-	 * @param hasRecycledMaterial - whether KUArchiects received recycled material or not.
-	 * @return - the total point of a KUArchitect
-	 */
+	
 	
 	private int pointCalculator(double knowledge, int experience,  int awards, boolean hasRecycledMaterial) {
         double sum=0.0;
@@ -147,48 +140,46 @@ public class KUArchitect extends ConsoleProgram {
 	}
 	
 	/**
-	 * This method assigns the total point calculator for a KUArchitects and assigns to the correct KUArchitect
-	 * @param p - the total point of any given KUArchitect
+	 * This method assigns the total point calculator for a Architects and assigns to the correct Architect
+	 * @param p - the total point of any given Architect
 	 */
 	private void assignPoint(int p) {
-        if(currentKUArchitectID==1) {
-        	totalPointOfKUArchitect1=pointCalculator(askKnowledge(),askExperience(),askAwards(),hasRecycledMaterial());//This part asks the knowledge,experience and awards for KUArchitect1.
-        	//Moreover, it assigns the total point of KU architect 1 to the point calculator for KUArchitect1. 
-        	println("KUArchitect#1 has reached "+totalPointOfKUArchitect1+" points.");
+        if(currentArchitectID==1) {
+        	totalPointOfArchitect1=pointCalculator(askKnowledge(),askExperience(),askAwards(),hasRecycledMaterial());//This part asks the knowledge,experience and awards for Architect1.
+        	
+        	println("Architect#1 has reached "+totalPointOfArchitect1+" points.");
         }
-        else if(currentKUArchitectID==2) {
-        	totalPointOfKUArchitect2=pointCalculator(askKnowledge(),askExperience(),askAwards(),hasRecycledMaterial());//This part asks the knowledge,experience and awards for KUArchitect2.
-        	//Moreover, it assigns the total point of KU architect 2 to the point calculator for KUArchitect2.
-        	println("KUArchitect#2 has reached "+totalPointOfKUArchitect2+" points.");
+        else if(currentArchitectID==2) {
+        	totalPointOfArchitect2=pointCalculator(askKnowledge(),askExperience(),askAwards(),hasRecycledMaterial());//This part asks the knowledge,experience and awards for Architect2.
+        	
+        	println("Architect#2 has reached "+totalPointOfArchitect2+" points.");
         }
-        else if(currentKUArchitectID==3) {
-        	totalPointOfKUArchitect3=pointCalculator(askKnowledge(),askExperience(),askAwards(),hasRecycledMaterial());//This part asks the knowledge,experience and awards for KUArchitect3.
-        	//Moreover, it assigns the total point of KU architect 3 to the point calculator for KUArchitect3.
-        	println("KUArchitect#3 has reached "+totalPointOfKUArchitect3+" points.");
+        else if(currentArchitectID==3) {
+        	totalPointOfArchitect3=pointCalculator(askKnowledge(),askExperience(),askAwards(),hasRecycledMaterial());//This part asks the knowledge,experience and awards for Architect3.
+        	
+        	println("Architect#3 has reached "+totalPointOfArchitect3+" points.");
         }
-        else if(currentKUArchitectID==4) {
-        	totalPointOfKUArchitect4=pointCalculator(askKnowledge(),askExperience(),askAwards(),hasRecycledMaterial());//This part asks the knowledge,experience and awards for KUArchitect4.
-        	//Moreover, it assigns the total point of KU architect 4 to the point calculator for KUArchitect4.
-        	println("KUArchitect#4 has reached "+totalPointOfKUArchitect4+" points.");
+        else if(currentArchitectID==4) {
+        	totalPointOfArchitect4=pointCalculator(askKnowledge(),askExperience(),askAwards(),hasRecycledMaterial());//This part asks the knowledge,experience and awards for Architect4.
+        	
+        	println("Architect#4 has reached "+totalPointOfArchitect4+" points.");
         }
 	}
 	
 	/**
 	 * This method compares the total point of all contestants and prints
-	 * the first, second and third order of the KUArchitect according to their total points.
+	 * the first, second and third order of the Architect according to their total points.
 	 */
-	private void comparator() {//In this method, I have incremented a,b,c,d and tried to find such a,b,c and d which satisfies the below condition that I write.
-		                       //I have assigned the a to the first ID,b to the secondID and c to the thirdID.Because, if j equals 1, totalPointOfKUArchitect(j) represents totalPointOfKUArchitect1.
-		                       //If j equals 2, it represents totalPointOfKUArchitect2. If j equals 3, it represents totalPointOfKUArchitect3.   
+	private void comparator() { 
 		// your code starts here
 	  for(int a=1;a<=4;a++) {
 		  for(int b=1;b<=4;b++) {
 			  for(int c=1;c<=4;c++) {
 				  for(int d=1;d<=4;d++) {//By using for loop, I have considered all possible cases for the firstPoint, secondPoint , thirdPoint, firstID, secondID, and thirdID.
-					  if(totalPointOfKUArchitect(a)>totalPointOfKUArchitect(b)&&totalPointOfKUArchitect(b)>totalPointOfKUArchitect(c)&&totalPointOfKUArchitect(c)>totalPointOfKUArchitect(d)) {
-			            firstPoint= totalPointOfKUArchitect(a);//Because, totalPointOfKUArchitect(a) is the greatest.
-						secondPoint=totalPointOfKUArchitect(b);
-						thirdPoint=totalPointOfKUArchitect(c);
+					  if(totalPointOfArchitect(a)>totalPointOfArchitect(b)&&totalPointOfArchitect(b)>totalPointOfArchitect(c)&&totalPointOfArchitect(c)>totalPointOfKUArchitect(d)) {
+			            firstPoint= totalPointOfArchitect(a);//Because, totalPointOfKUArchitect(a) is the greatest.
+						secondPoint=totalPointOfArchitect(b);
+						thirdPoint=totalPointOfArchitect(c);
 						firstID=a;
 						secondID=b;
 						thirdID=c;
@@ -197,9 +188,9 @@ public class KUArchitect extends ConsoleProgram {
 				}
 			}
 		}
-	println("KUArchitect#"+firstID+" becomes #1 with "+firstPoint+" points.");
-	println("KUArchitect#"+secondID+" becomes #2 with "+secondPoint+" points.");
-	println("KUArchitect#"+thirdID+" becomes #3 with "+thirdPoint+" points.");
+	println("Architect#"+firstID+" becomes #1 with "+firstPoint+" points.");
+	println("Architect#"+secondID+" becomes #2 with "+secondPoint+" points.");
+	println("Architect#"+thirdID+" becomes #3 with "+thirdPoint+" points.");
 	println();
 	}
 	
@@ -245,22 +236,22 @@ public class KUArchitect extends ConsoleProgram {
 			return(((int)z)+1);//If the z value has a decimal point greater than or equal to 0.5, round method returns the ((integer form of z value)+1).
 	}
 
-	private int totalPointOfKUArchitect(int j) {
+	private int totalPointOfArchitect(int j) {
 		// TODO Auto-generated method stub
 		if(j==1) {
-			return totalPointOfKUArchitect1; //totalPointOfKUArchitect(1)=totalPointOfKUArchitect1
+			return totalPointOfArchitect1; 
 		}
 		else if(j==2) {
-			return totalPointOfKUArchitect2;//totalPointOfKUArchitect(2)=totalPointOfKUArchitect2
+			return totalPointOfArchitect2;
 		}
 		else if(j==3) {
-			return totalPointOfKUArchitect3;//totalPointOfKUArchitect(3)=totalPointOfKUArchitect3
+			return totalPointOfArchitect3;
 		}
-		return totalPointOfKUArchitect4;//totalPointOfKUArchitect(4)=totalPointOfKUArchitect4
+		return totalPointOfArchitect4;
 	}
 	private void battleAndWinNumbers() {//This method creates a battle among the contestants who are first three according to their points. 
 		 //Contestants' winning numbers are proportional to their winning probability against each other. 
-		 //I have incremented the win numbers according to the KU Architects' winning probablities against each other.
+		 //I have incremented the win numbers according to the  Architects' winning probablities against each other.
 		 //This method prints out how many times each architect won in 300 matches.
 		    println("The game is completed now and the scores are as below: ");
 			println();
@@ -288,41 +279,41 @@ public class KUArchitect extends ConsoleProgram {
 					num3++;
 				}
 			}
-			println("KUArchitect#"+firstID+" won "+num1+" times in "+3*NTIMES+" matches.");
-			println("KUArchitect#"+secondID+" won "+num2+" times in "+3*NTIMES+" matches.");
-			println("KUArchitect#"+thirdID+" won "+num3+" times in "+3*NTIMES+" matches.");
+			println("Architect#"+firstID+" won "+num1+" times in "+3*NTIMES+" matches.");
+			println("Architect#"+secondID+" won "+num2+" times in "+3*NTIMES+" matches.");
+			println("Architect#"+thirdID+" won "+num3+" times in "+3*NTIMES+" matches.");
 	 }
 			
 	 private void congratulations() {
 			println();
 			if(num3>num1&&num1>num2) {
-				println("CONGRATULATIONS KUArchitect#"+thirdID+"!! YOU ARE THE WINNER OF KUArchitect.");
+				println("CONGRATULATIONS Architect#"+thirdID+"!! YOU ARE THE WINNER OF Architect.");
 			}
 			else if(num3>num2&&num2>num1) {
-				println("CONGRATULATIONS KUArchitect#"+thirdID+"!! YOU ARE THE WINNER OF KUArchitect.");
+				println("CONGRATULATIONS Architect#"+thirdID+"!! YOU ARE THE WINNER OF Architect.");
 			}
 			else if(num2>num1&&num1>num3) {
-				println("CONGRATULATIONS KUArchitect#"+secondID+"!! YOU ARE THE WINNER OF KUArchitect.");
+				println("CONGRATULATIONS Architect#"+secondID+"!! YOU ARE THE WINNER OF Architect.");
 			}
 			else if(num2>num3&&num3>num1) {
-				println("CONGRATULATIONS KUArchitect#"+secondID+"!! YOU ARE THE WINNER OF KUArchitect.");
+				println("CONGRATULATIONS Architect#"+secondID+"!! YOU ARE THE WINNER OF Architect.");
 			}
 			else if(num1>num2&&num2>num3) {
-				println("CONGRATULATIONS KUArchitect#"+firstID+"!! YOU ARE THE WINNER OF KUArchitect.");
+				println("CONGRATULATIONS Architect#"+firstID+"!! YOU ARE THE WINNER OF Architect.");
 			}
 			else if(num1>num3&&num3>num2) {
-				println("CONGRATULATIONS KUArchitect#"+firstID+"!! YOU ARE THE WINNER OF KUArchitect.");
+				println("CONGRATULATIONS Architect#"+firstID+"!! YOU ARE THE WINNER OF Architect.");
 			}
 	  }
 
 	// your code ends here
 	
 	//////////// GIVEN VARIABLES and CONSTANTS /////////////////////
-	int currentKUArchitectID = 1;
-	int totalPointOfKUArchitect1;
-	int totalPointOfKUArchitect2;
-	int totalPointOfKUArchitect3;
-	int totalPointOfKUArchitect4;
+	int currentArchitectID = 1;
+	int totalPointOfArchitect1;
+	int totalPointOfArchitect2;
+	int totalPointOfArchitect3;
+	int totalPointOfArchitect4;
 	int firstID;
 	int secondID;
 	int thirdID;
